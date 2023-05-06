5f8b6cc3ce90640826ff02a05a9701c651442767
Ankit952077 <116968419+Ankit952077@users.noreply.github.com>
2023-05-06 05:49:50 -0400

    Table print

---
+++ 01.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter n\n");
+    scanf("%d",&n);
+
+ for (int i = 1; i <=10; i++)
+ {
+    printf("table is :=%d\n",n*i);
+ }
+ return 0;
+
+}
